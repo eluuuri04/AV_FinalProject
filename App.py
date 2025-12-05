49bdc0e007776bdae9df0d1d9d9dfc9d2a490c20
eluuuri04 <uriel.cabanas01@estudiant.upf.edu>
2025-12-05 15:16:37 +0100

    Inicial docs

---
+++ App.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+
+st.markdown("## <span style='color:deepskyblue'>Predictor d'abandonament universitari</span>", unsafe_allow_html=True)
+st.markdown("## <span style='color:goldenrod'>Projecte Final d'Analítica Visual</span>", unsafe_allow_html=True)
+st.markdown("#### <span style='color:mediumseagreen'>Benvingut/da!</span>", unsafe_allow_html=True)
+
+st.markdown("""
+**Nom:** Uriel Cabañas Pedro  
+**NIA:** 269121
+           
+**Nom:** Pau Colomer Coll  
+**NIA:** -------
+
+            
+En aquesta aplicació web podràs:
+- **Explorar** les dades històriques dels estudiants (DIR QUINS ESTUDIANTS SÓN).
+- **Predir** si un estudiant concret abandonarà o seguirà els seus estudis.
+- **Descobrir** quins són els factors, de la predicció, més rellevants de cada estudiant.
+""")
+
